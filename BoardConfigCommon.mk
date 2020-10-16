fcuzzocrea/android_device_samsung_universal8890-common

universal8890: set recovery fstab
Change-Id: I077bddba045a7b8d037ede24e7b41dad72bfecd6

BoardConfigCommon.mk
@@ -126,6 +126,9 @@ TARGET_BOARD_PLATFORM := exynos5
 TARGET_SLSI_VARIANT := bsp
 TARGET_BOOTLOADER_BOARD_NAME := universal8890
 
+# Recovery
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/ramdisk/etc/fstab.samsungexynos8890
+
 # Scaler
 BOARD_USES_DEFAULT_CSC_HW_SCALER := true
 BOARD_USES_DT := true

root2raj/device_samsung_hero-common

BoardConfigCommon.mk
@@ -105,6 +105,9 @@ TARGET_SOC := exynos8890
 # Properties
 TARGET_SYSTEM_PROP := $(COMMON_PATH)/system.prop
 
+# Recovery
+TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/ramdisk/etc/fstab.samsungexynos8890
+
 # Renderscript
 OVERRIDE_RS_DRIVER := libRSDriverArm.so
 