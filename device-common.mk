fcuzzocrea/android_device_samsung_universal8890-common

universal8890: Add common overlays
Change-Id: I99e636f87cdfb20cea34090ce74d51dfaf413873

device-common.mk
@@ -16,6 +16,8 @@
 
 LOCAL_PATH := device/samsung/universal8890-common
 
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 # Audio
 PRODUCT_PACKAGES += \
     audio.a2dp.default \

root2raj/device_samsung_hero-common

device-common.mk
@@ -16,6 +16,9 @@
 
 COMMON_PATH := device/samsung/universal8890-common
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(COMMON_PATH)/overlay
+
 # Boot animation
 TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_TEXTURE_CACHE := true